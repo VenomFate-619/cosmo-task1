257ce3d721edf65a15946633698db834cefd67ef
VenomFate-619 <aliamaan119@gmail.com>
2023-04-19 15:27:31 +0530

    new:change select tag style

--- src/components/field/index.tsx
+++ src/components/field/index.tsx
@@ -35,27 +35,35 @@ const Field = ({
     <>
       <div className="flex col-start-2 col-end-3 border-b border-gray-400">
         <div className="flex justify-between w-full rounded group hover:bg-[#EDEAEC] p-2">
-          <div className="space-x-2 flex">
-            <InlineTextEditor text={nameOfField} saveName={updateField} id={id} />
+          <div className="space-x-2 flex items-center">
+            <InlineTextEditor
+              text={nameOfField}
+              saveName={updateField}
+              id={id}
+            />
             <select
               value={typeOfField}
               onChange={(e) => changeType(id, e.target.value as typesOfData)}
+              className="bg-gray-200 py-1 font-medium rounded-lg outline-none focus:outline-none"
             >
-              <option value="string">String</option>
-              <option value="number">Number</option>
-              <option value="boolean">Boolean</option>
-              <option value="object">Object</option>
+              {Object.values(typesOfData).map((value) => (
+                <option value={value} key={value}>
+                  {value.toUpperCase()}
+                </option>
+              ))}
             </select>
           </div>
           <div className="group-hover:flex items-center gap-x-2 hidden">
-            <div className="flex gap-x-2 items-center" >
+            <div className="flex gap-x-2 items-center">
               <span>Required</span>
               <label className="relative inline-flex items-center  cursor-pointer">
                 <input
                   type="checkbox"
                   checked={schemaData[id].required}
                   className="sr-only peer"
-                  onChange={(e)=> updateField(id , {required:e.target.checked})}
+                  onChange={(e) =>
+                    updateField(id, { required: e.target.checked })
+                  }
                 />
                 <div className="w-9 h-5 bg-gray-200 peer-focus:outline-none  dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600" />
               </label>
